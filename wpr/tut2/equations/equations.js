536dc6d8fc4d0fb1225f8507356a1c647e263c90
Phuc <mrp751024@gmail.com>
2026-07-21 04:16:04 +0700

    Hide 'solve' button

--- wpr/tut2/equations/equations.js
+++ wpr/tut2/equations/equations.js
@@ -5,11 +5,10 @@
  * Use-case scenario: when a user enters numbers on three text fields with ids of "a",
  * "b", "c" and clicks "Solve" button, equation solution will be displayed on the <div>
  * with id "result".
- * 
+ *
  */
- "use strict";
-(function() {
-
+"use strict";
+(function () {
   window.addEventListener("load", init);
 
   /**
@@ -24,30 +23,30 @@
     btn.addEventListener("click", solve);
 
     function solve() {
+      btn.classList.add("hide");
+
       const a = inputA.valueAsNumber;
       const b = inputB.valueAsNumber;
       const c = inputC.valueAsNumber;
 
       const result = document.getElementById("result");
-      
+
       if (isNaN(a) || isNaN(b) || isNaN(c)) {
-        result.textContent = "Please enter correct numbers"
+        result.textContent = "Please enter correct numbers";
       } else {
         let delta = b * b - 4 * a * c;
 
-         if (delta < 0) {
-        result.textContent = "No solution";
-    } 
-    else if (delta === 0) {
-        let x = -b / (2 * a);
-        result.textContent = `x = ${x}`;
-    } 
-    else {
-        let x1 = (-b + Math.sqrt(delta)) / (2 * a);
-        let x2 = (-b - Math.sqrt(delta)) / (2 * a);
+        if (delta < 0) {
+          result.textContent = "No solution";
+        } else if (delta === 0) {
+          let x = -b / (2 * a);
+          result.textContent = `x = ${x}`;
+        } else {
+          let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+          let x2 = (-b - Math.sqrt(delta)) / (2 * a);
 
-        result.textContent = `x1 = ${x1}, x2 = ${x2}`;
-    }
+          result.textContent = `x1 = ${x1}, x2 = ${x2}`;
+        }
       }
     }
   }
