d8b6c0c0e1e15aabc3c325e7c5fa557d9b3937f0
Phuc <mrp751024@gmail.com>
2026-07-25 07:32:24 +0700

    fix pokeball

--- wpr/tut2/pokeball/pokeball.js
+++ wpr/tut2/pokeball/pokeball.js
@@ -8,12 +8,13 @@
 (function() {
 
   window.addEventListener("DOMContentLoaded", () => {
-    let btn = document.getElementById("demo-btn");
-    btn.addEventListener("click", init);
+    document.getElementById("demo-btn")
+    .addEventListener("click", init);
   });
 
   function init() {
    let ball = document.getElementById("pokeball")
+   document.getElementById("demo-btn").disabled = true;
    ball.src = "mystery.gif";
    ball.alt = "your new pokemon"
   }
