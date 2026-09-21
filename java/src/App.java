617eafeb35daacb2037746162fa2ad034ee05e02
Phuc <mrp751024@gmail.com>
2026-09-21 21:52:13 +0700

    Game Window

--- java/src/App.java
+++ java/src/App.java
@@ -1,15 +1,18 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class App {
     public static void main(String [] args) throws Exception {
         int rowCount = 21;
         int columnCount = 19;
         int titleSize = 32;
-        int boardHeigh = rowCount * titleSize;
+        int boardHeight = rowCount * titleSize;
         int boardWidth = columnCount * titleSize;
 
         JFrame frame = new JFrame("Pac Man");
         frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
